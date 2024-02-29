14911e1d14117290ead3ba7bcb8ce3bd0039fb71
Ruchit <risenid@duck.com>
2024-03-01 02:31:48 +0530

    veux: overlay: Spoof GMS to griffin_retcn
    thanks to Legendleo90
    
    Change-Id: I2a24a72a723ed99623c18eca1c7fac1903b0deaf
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- device.mk
+++ device.mk
@@ -277,7 +277,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayCOMMON \
-    DialerOverlayCOMMON \
+    FrameworkOverlayCOMMON \
     TelephonyOverlayCOMMON
 
 PRODUCT_PACKAGES += \
