cb36c0021505f6f32ea45c27f459d55dce204150
alexandrusemina <alexandrusemina8@gmail.com>
2024-03-17 16:27:44 +0200

    veux: Prepare for Derpfest

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+echo "Cloning kernel..."
+git clone https://github.com/alexandrusemina/kernel_xiaomi_sm6375.git kernel/xiaomi/sm6375 --depth=1
+
+echo "Cloning Vendor..."
+git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_veux vendor/xiaomi/veux
+
+echo "Cloning MiCam"
+git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_miuicamera-veux vendor/xiaomi/miuicamera-veux
+
+echo "Cloning hardware related stuff..."
+git clone https://github.com/PixelExperience/hardware_xiaomi hardware/xiaomi
