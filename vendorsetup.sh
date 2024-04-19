feb69059f0f2ffdbb698add39d9b9c07f3a692cc
alexandrusemina <alexandrusemina8@gmail.com>
2024-04-19 14:59:21 +0300

    vendorsetup: include prebuild kernel repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 echo "Cloning kernel..."
 git clone https://github.com/alexandrusemina/kernel_xiaomi_sm6375.git kernel/xiaomi/sm6375 --depth=1
-
+git clone https://github.com/PixelExperience-Devices/device_xiaomi_veux-kernel.git device/xiaomi/veux-kernel
 echo "Cloning Vendor..."
 git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_veux vendor/xiaomi/veux
 
