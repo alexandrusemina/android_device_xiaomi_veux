387e7e38b21eadb5c0a3174af42bc04892e5a37d
alexandrusemina <alexandrusemina8@gmail.com>
2024-04-21 16:13:43 +0300

    vendorsetup: replace vendor repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,9 @@
 echo "Cloning kernel..."
 git clone https://github.com/alexandrusemina/kernel_xiaomi_sm6375.git kernel/xiaomi/sm6375 --depth=1
 git clone https://github.com/PixelExperience-Devices/device_xiaomi_veux-kernel.git device/xiaomi/veux-kernel
+
 echo "Cloning Vendor..."
-git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_veux vendor/xiaomi/veux
+git clone https://github.com/alexandrusemina/vendor_xiaomi_veux.git vendor/xiaomi/veux
 
 echo "Cloning MiCam"
 git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_miuicamera-veux vendor/xiaomi/miuicamera-veux
